a69763d99d50ff7ff03ae3728a58f12b3713cf0b
Daiva.U <daiva.urn@gmail.com>
2018-10-03 12:08:35 +0300

    dec

--- DaivaU/index.php
+++ DaivaU/index.php
@@ -20,8 +20,6 @@ if(!empty($_POST)) {
       file_put_contents($file, $current);
   
     }elseif(isset($_POST['nuskaityti']) ){ //Jei ne pradinis įrašymas tai reikia esamam redaguoti
-      //Ar githubas dirba, kaip gDrive?
-      
       $file = $_POST['file_name'].'.txt';
       $from_file = file_get_contents($file);
   
@@ -31,14 +29,37 @@ if(!empty($_POST)) {
       $text = '';
       file_put_contents($newFile, $text);
     }
+   
+  $text_area = '<form action="" method = "post">
+  <textarea type="text" name="message" style="width:400px; height:200px;" >'.$from_file.'</textarea>
+  <br>
+  <input type="submit" name="button" value="Įrašyti">
+  <input style="width:100px" type="text" name="file_name" value="'.$file.'" hidden>
   
-    }elseif( isset($_GET['failas'])){ //Jei POSTO NERA TADA GAL GETAS YRA
-    //Ar githubas dirba, kaip gDrive?
-    
+  <br><br><br>
+  </form>';
+  echo $text_area;
+
+}elseif( isset($_GET['failas'])){ //Jei POSTO NERA TADA GAL GETAS YRA
+
+  if(substr($_GET['failas'], -4, 4) == '.txt'){
     $file = $_GET['failas'];
     $from_file = file_get_contents($file);
-  
+    $text_area = '<form action="" method = "post">
+    <textarea type="text" name="message" style="width:400px; height:200px;" >'.$from_file.'</textarea><br>
+    <input type="submit" name="button" value="Įrašyti">
+    <input style="width:100px" type="text" name="file_name" value="'.$file.'" hidden>
+    <br><br><br>
+    </form>';
+    echo $text_area;
+
+  }elseif(substr($_GET['failas'], -4, 4) == '.jpg'){
+    echo '<img style="width:300px" src="/folder/'.$_GET['failas'].'">';
   }
+    
+}else{
+  echo $text_area;
+}
   
 // //uzduotis 4
 // if ($handle = opendir('.')) {
@@ -51,37 +72,21 @@ if(!empty($_POST)) {
 //   closedir($handle);
 // }
 
-
 $dir = __DIR__;
+// echo $dir2 = __DIR__.'/folder';
 
 if (is_dir($dir)) {
   if ($dh = opendir($dir)) {
   while (($fileList = readdir($dh)) !==false) {
-      if (substr($fileList, -4, 4) == '.txt' || substr($fileList, -4, 4) == '.jpg') {
+      if ( substr($fileList, -4, 4) == '.txt'|| substr($fileList, -4, 4) == '.jpg') {
         $sarasas .= '<li><a href="?failas='.$fileList.'" >'.$fileList.'</a> </li>';
        }
     }
   } closedir($dh);
 }
-//uzduotis 5
-
-
-
-
-
-
 
 ?>
 
-
-<form action="" method = "post">
-    <textarea type="text" name="message" style="width:600px; height:300px;" ><?= $from_file ?></textarea>
-    <br>
-    <input type="submit" name="button" value="Įrašyti">
-    <input style="width:100px" type="text" name="file_name" value="<?= $file ?>" hidden>
-
-    <br><br><br>
-</form>
 <h3>Jūs galite pasirinkti iš:</h3><ul><?= $sarasas ?></ul>
 
 <form action="" method = "post">
@@ -97,4 +102,4 @@ if (is_dir($dir)) {
 </form>
 
 <?php
-
+?>
